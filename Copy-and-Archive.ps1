651257be14d83957bf22234759f88b44b70cc6f1
soulphish <56435520+soulphish@users.noreply.github.com>
2019-11-11 10:23:39 +0000

    Update Copy-and-Archive.ps1

--- Copy-and-Archive.ps1
+++ Copy-and-Archive.ps1
@@ -1,1 +1,16 @@
-Write-Output "Hello World"
+$sourceDir = "L:\Data\IS Info\2019 Projects\MPOD\J-Block Remediation\2008-Remidiation"
+$archiveDir = $sourceDir + "\Archive"
+$files = Get-ChildItem $sourceDir | Where-Object { $_.Name -like 'Jblock-mPOD_Migration*' }
+$dateMinus = Get-Date
+$dateMinus = $dateMinus.AddDays(-1)
+$dateMinus = $dateMinus.ToString("yyyy-MM-dd")
+
+Set-Location $sourceDir
+
+foreach ($file in $files)
+{
+
+    $archiveFile = $archiveDir + "\" + $file.BaseName + "_" + $dateMinus + $file.Extension
+    Copy-Item $file $archiveFile
+
+}
